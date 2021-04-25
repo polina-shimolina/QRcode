d3b11afb33834d7a28272083a56b9d7387de0133
polina-shimolina <64307985+polina-shimolina@users.noreply.github.com>
2021-04-25 15:21:41 +0300

    1

---
+++ QR/QR/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace QR
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //если текст
+            textbox.Visibility = Visibility.Visible;
+            string qrtext = textbox.Text; 
+            QRCodeEncoder encoder = new QRCodeEncoder(); //создаем объект класса QRCodeEncoder
+            Bitmap qrcode = encoder.Encode(qrtext); // кодируем слово, полученное из TextBox'a (qrtext) в переменную qrcode. класса Bitmap(класс, который используется для работы с изображениями)
+            pictureBox1.Image = qrcode as Image;
+        }
+    }
+}
