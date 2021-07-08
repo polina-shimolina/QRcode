d61fe31ad191543b5f02e11d8c41218c6862d55d
polina-shimolina <64307985+polina-shimolina@users.noreply.github.com>
2021-07-09 02:34:48 +0300

    5

---
+++ QRnew/QRnew/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Controls;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ZXing;
+using ZXing.QrCode;
+using System.Drawing;
+
+namespace QRnew
+{
+
+    public partial class MainWindow : Window
+    {
+        QR qr = new QR();
+        ImageSource qrimage;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void QRType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (QRType.SelectedItem)
+            {
+                case -1:
+                    break;
+                case 0: //text
+                case 1: //url
+                    {
+                        Text.Visibility = Visibility.Visible;
+                        qrimage = qr.textOrURL(Text.Text.Trim());
+                        break;
+                    }
+                case 2:
+                    break;
+                case 3: //pic
+                    break;
+                default:
+                    break;
+            }
+
+            Text.Visibility = Visibility.Visible;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            image.Source = qrimage;  //
+            Text.Clear();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            qr.SaveQR();
+        }
+    }
+}
---
+++ QRnew/QRnew/QR.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ZXing;
+using ZXing.QrCode;
+using System.Drawing;
+using Microsoft.Win32;
+
+namespace QRnew
+{
+    class QR
+    {
+        private int width, height; //
+        private QrCodeEncodingOptions options;
+        public QR()
+        {
+            width = 300;
+            height = 300;
+            options = new QrCodeEncodingOptions { DisableECI = true, CharacterSet = "UTF-8", Width = width, Height = height };
+        }
+        public QR(int w, int h)
+        {
+            width = w;
+            height = h;
+            options = new QrCodeEncodingOptions { DisableECI = true, CharacterSet = "UTF-8", Width = width, Height = height };
+        }
+        //public ~QR() { }
+        public ImageSource textOrURL(string TeXt)
+        {
+            BarcodeWriter qr = new BarcodeWriter();
+            qr.Options = options;
+            qr.Format = ZXing.BarcodeFormat.QR_CODE;
+            Bitmap result = new Bitmap(qr.Write(TeXt));
+            BitmapSource b = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(result.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            ImageSource IM = b;
+            return b;
+        }
+
+        public void SaveQR()
+        {
+            SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "QR"; // Default file name
+            dlg.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp"; // Filter files by extension
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+            }
+        }
+    }
+}
